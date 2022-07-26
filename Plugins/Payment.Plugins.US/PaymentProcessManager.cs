567364a3e74b5d654202124078836f9255d932ff
bm1905 <bijay.maharjan5@gmail.com>
2022-07-25 23:35:30 -0500

    Implement Factory Pattern

---
+++ Plugins/Payment.Plugins.US/PaymentProcessManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using GenericMediatRPattern.PaymentValidationProcessor;
+using Microsoft.Extensions.Configuration;
+
+namespace Payment.Plugins.US
+{
+    public class PaymentProcessManager : IPaymentValidationProcessor
+    {
+        private readonly IConfiguration _configuration;
+
+        public PaymentProcessManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<PaymentValidationProcessorResponse> ValidateAndProcessPayment(string key)
+        {
+            // Operation based on Locale
+            await Task.Delay(1);
+            int random = new Random().Next();
+            PaymentValidationProcessorResponse response = new PaymentValidationProcessorResponse
+            {
+                Locale = "US",
+                IsValid = random % 2 == 0
+            };
+
+            return response;
+        }
+    }
+}
